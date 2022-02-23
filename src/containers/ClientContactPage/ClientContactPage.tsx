69fb84ec9d2e501af0ce297f4cd8dc9c12105d25
stefoverflow <stefan.krstic@proceedix.com>
2022-02-23 21:59:52 +0100

    feature(menu):

--- src/containers/ClientContactPage/ClientContactPage.tsx
+++ src/containers/ClientContactPage/ClientContactPage.tsx
@@ -9,6 +9,7 @@ import './ClientContactPage.scss';
 import { composeValidators, emailFormatValid, required } from '../../util/validators';
 import agent from '../../api/agent';
 import { Redirect, useHistory } from 'react-router-dom';
+import MenuButton from '../../components/MenuButton/MenuButton';
 
 interface ClientContactPageType {};
 
@@ -155,11 +156,7 @@ const ClientContactPage: React.FC<ClientContactPageType> = () => {
             )
           }
         />
-        <div className="landing-client__menu-button">
-            <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M18 12H6V10H18V12ZM18 7H0V5H18V7ZM18 2H6V0H18V2Z" fill="white"/>
-            </svg>
-        </div>
+        <MenuButton />
         </div>
     </div>   
 };
