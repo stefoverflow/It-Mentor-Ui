b3f43fbface248f78261c9b9d4c4b863eaa7dc27
Stefan <stefantosic.dev@gmail.com>
2022-03-25 13:03:58 +0100

    slight footer fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,60 +1,49 @@
-import React from 'react';
-import WatermarkLogo from '../WatermarkLogo/WatermarkLogo';
+import React from "react";
+import WatermarkLogo from "../WatermarkLogo/WatermarkLogo";
 
-import './Footer.scss';
+import "./Footer.scss";
 
 const Footer = () => {
-
-    return <div style={{ position: 'relative', marginTop: '23.1rem' }}>
-        <WatermarkLogo className="footer__watermark"/>
-    <div className="footer">
+  return (
+    <div style={{ position: "relative" }}>
+      <WatermarkLogo className="footer__watermark" />
+      <div className="footer">
         <div className="footer__main-column">
-            <div className="footer__main-column__title">
-                Glavni naslov
+          <div className="footer__main-column__title">Glavni naslov</div>
+          <div className="footer__main-column__sub-column">
+            <div className="footer__main-column__sub-column__link">
+              Neki tekst
             </div>
-            <div className="footer__main-column__sub-column">
-                <div className="footer__main-column__sub-column__link">
-                    Neki tekst
-                </div>
-                <div className="footer__main-column__sub-column__link">
-                    Isto
-                </div>
-                <div className="footer__main-column__sub-column__link">
-                    Podnaslov
-                </div>
+            <div className="footer__main-column__sub-column__link">Isto</div>
+            <div className="footer__main-column__sub-column__link">
+              Podnaslov
             </div>
+          </div>
         </div>
         <div className="footer__main-column">
-            <div className="footer__main-column__title">
-               Legal info
+          <div className="footer__main-column__title">Legal info</div>
+          <div className="footer__main-column__sub-column">
+            <div className="footer__main-column__sub-column__link">License</div>
+            <div className="footer__main-column__sub-column__link">
+              www.itachi.rs
             </div>
-            <div className="footer__main-column__sub-column">
-                <div className="footer__main-column__sub-column__link">
-                    License
-                </div>
-                <div className="footer__main-column__sub-column__link">
-                    www.itachi.rs
-                </div>
-            </div>
+          </div>
         </div>
         <div className="footer__main-column">
-            <div className="footer__main-column__title">
-                Follow us
+          <div className="footer__main-column__title">Follow us</div>
+          <div className="footer__main-column__sub-column">
+            <div className="footer__main-column__sub-column__link">
+              Linkedin
             </div>
-            <div className="footer__main-column__sub-column">
-                <div className="footer__main-column__sub-column__link">
-                    Linkedin
-                </div>
-                <div className="footer__main-column__sub-column__link">
-                    Instagram
-                </div>
-                <div className="footer__main-column__sub-column__link">
-                    Twitter
-                </div>
+            <div className="footer__main-column__sub-column__link">
+              Instagram
             </div>
+            <div className="footer__main-column__sub-column__link">Twitter</div>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
+  );
 };
 
 export default Footer;
